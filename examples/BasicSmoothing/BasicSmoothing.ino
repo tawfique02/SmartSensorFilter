dec11f3fbe0213fea44e46aedcb4e30be793ec91
Tawfique Elahey <177960094+tawfique02@users.noreply.github.com>
2026-07-27 23:16:23 +0600

    Create BasicSmoothing.ino

---
+++ examples/BasicSmoothing/BasicSmoothing.ino
@@ -0,0 +1,25 @@
+#include <SmartSensorFilter.h>
+
+SmartSensorFilter filter(10);
+
+void setup()
+{
+    Serial.begin(9600);
+    filter.begin();
+}
+
+void loop()
+{
+
+    int rawData = random(50, 80);
+
+    int cleanData = filter.update(rawData);
+
+    Serial.print("Raw:");
+    Serial.print(rawData);
+    Serial.print(",");
+    Serial.print("Filtered:");
+    Serial.println(cleanData);
+
+    delay(100);
+}
